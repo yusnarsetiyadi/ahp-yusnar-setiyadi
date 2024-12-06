b22814aa419af711ea27c3f170c8f2e9cbec4e26
yusnarsetiyadi <yusnarsetiyadi150403@gmail.com>
2024-12-06 15:03:57 +0700

    rebuild

--- db.php
+++ db.php
@@ -4,6 +4,11 @@ $username = "root";
 $password = "";
 $database = "ahp_db";
 
+// $servername = "103.84.207.118";
+// $username = "yusnar";
+// $password = "Yusnar12345*";
+// $database = "ahp_db";
+
 $conn = new mysqli($servername, $username, $password, $database);
 
 if ($conn->connect_error) {
